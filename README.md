7594f369587c02ff5a8f15423b10240ba868538d
stevenkim18 <11882@naver.com>
2021-03-11 19:19:13 +0900

    feat: ch2 완료 슬라이더, 버튼 이벤트 제어

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # ios Up and Down Game
 본 프로젝트는 야곰의 `왕초보를 위한 ios 강의`를 보고 작성했습니다.   
 강의 링크 https://yagom.net/courses/ios-starter-uikit/
+
+## UIkit
+- User Interface kit
+- 사용자와 상호작용할 모든 것들이 보여 있는 킷
+
+### 오브젝트 라이브러리 열기
+- cmd + shift + L
+
+### IBOutlet
+- Ui의 요소를 사용하고 싶을 때 사용
+- button, label, slider 등등
+
+### IBAction
+- ui의 요소에 이벤트들을 처리하고 싶을 때 사용
+
+## Event Driven Programming
+- 이벤트 기반 프로그래밍
+- 사용자의 이벤트나 시스템의 이벤트가 발생하면 동작하는 방식
+- 이벤트가 없으면 프로그램이 무작정 이벤트를 기다림.
--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton() {
+        print("slider", slider.value)
+    }
+    
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        print("touch up reset button")
+    }
+    
 
 }
 
