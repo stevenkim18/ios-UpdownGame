2cf5300f7ee62d2f4f4e1b9cfa8abcf09a86e774
stevenkim18 <11882@naver.com>
2021-03-16 16:00:03 +0900

    feat: 내가 혼자 만들어 봄. 게임 가능함.

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,26 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomNumber: Int = 0
+    var tryCount: Int = 0
+    
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maxinumValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
+        
+        reset()
     }
     
     @IBAction func sliderValueChanged(_ sender: UISlider) {
-        print(sender.value)
+        sliderValueLabel.text = String(Int(sender.value))
     }
     
     @IBAction func touchUpHitButton() {
-        print("slider", slider.value)
+        tryCount += 1
+        tryCountLabel.text = String(tryCount) + " / 5"
+        print("slider", slider.value, "random", randomNumber)
+        if Int(slider.value) < randomNumber {
+            minimumValueLabel.text = String(Int(slider.value))
+            slider.minimumValue = slider.value
+        } else if Int(slider.value) > randomNumber {
+            maxinumValueLabel.text = String(Int(slider.value))
+            slider.maximumValue = slider.value
+        } else {
+            let alert = UIAlertController(title: "맞췄습니다!", message: nil, preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(defaultAction)
+            present(alert, animated: false, completion: reset)
+        }
     }
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
         print("touch up reset button")
+        reset()
     }
     
-
+    func reset() {
+        print("reset")
+        randomNumber = Int.random(in: 0...30)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maxinumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
+    }
 }
-
